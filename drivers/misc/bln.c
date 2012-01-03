a75934daf3d99f2afe04384c0171fcd6c83963c9
Ezekeel <notezekeel@googlemail.com>
2012-01-03 23:06:39 +0100

    By default BLN is disabled.

--- drivers/misc/bln.c
+++ drivers/misc/bln.c
@@ -18,7 +18,7 @@
 #include <linux/timer.h>
 #include <linux/wakelock.h>
 
-static bool bln_enabled = true; /* is BLN function is enabled */
+static bool bln_enabled = false; /* is BLN function is enabled */
 static bool bln_ongoing = false; /* ongoing LED Notification */
 static int bln_blink_state = 0;
 static bool bln_suspended = false; /* is system suspended */
