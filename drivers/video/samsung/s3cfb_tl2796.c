86196e899b7e29d8fcb0fb1f460a8f33fb9291a0
Ezekeel <notezekeel@googlemail.com>
2012-01-27 02:51:30 +0100

    The gamma V1 offset is initialized to the default stock values.

--- drivers/video/samsung/s3cfb_tl2796.c
+++ drivers/video/samsung/s3cfb_tl2796.c
@@ -77,7 +77,7 @@ struct s5p_lcd *lcd_;
 u32 original_color_adj_mults[3];
 unsigned int panel_config_sequence = 0;
 
-int hacky_v1_offset[3] = {-14, -17, -18};
+int hacky_v1_offset[3] = {0, 0, 0};
 
 static const u16 s6e63m0_SEQ_ETC_SETTING_SAMSUNG[] = {
 	/* ETC Condition Set Command  */
